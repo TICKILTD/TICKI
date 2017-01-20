5df13535e9961490b1b78f3a4a416371db70710b
siparsons <simon.parsons@retig.co.uk>
2017-01-20 12:09:13 +0000

    Introduced save function

--- app.js
+++ app.js
@@ -10,39 +10,79 @@ var $ = require("jquery");
 
 class DPA {
 
-    constructor(container, options) {
-        this.options = options;
-        this.container = container;
+    constructor(tenant_id) {
+        this.tenant_id = tenant_id;
     }
 
-    init() {
+    init(container, config) {
 
-    	var self = this;
+        var self = this;
+
+        self.config = config;
+        self.container = container;
 
         if (self.container) {
 
-			$.getJSON( "http://localhost:8080/api/questions")
-			    .done(function(questions) {
+            $.getJSON( "http://localhost:8080/api/questions")
+                .done(function(questions) {
 
-		    		var html = "<input type=\"checkbox\" value=\"{id}\"> <strong>{text}</strong> <br />";
+                    self.questions = $.map(questions, function(q) { return q._id; });
 
-			      	$.each(questions, function( i, question ) {
-			
-						var out = html.replace(/\{id\}/, question.id)
-									  .replace(/\{text\}/, question.text);
+                    var input_css = "";
+                    if (self.config.input_css) input_css = " class=\"" + self.config.input_css + "\"";
 
-						$(self.container).append(out);
-			      	});
-			    });
+                    var html = "<input id=\"{id}\"" + input_css + " type=\"checkbox\"> <strong>{text}</strong> <br />";
+
+                    $.each(questions, function( i, question ) {
+            
+                        var out = html.replace(/\{id\}/, question._id)
+                                      .replace(/\{text\}/, question.text);
+
+                        $(self.container).append(out);
+                    });
+                });
         }
     }
 
-    post() {
-    	var self = this;
+    save(submission, onSubmit) {
 
-    	if (self.container) {
+        var self = this;
 
-    	}
+        if (self.container) {
+
+            var questionControls = $(self.questions.reduce(function(a, b) {
+                return ((!/^\#/.test(a)) ? '#' + a : a) + ', #' + b;
+            }));
+
+            var payload = {
+                tenant_id   : self.tenant_id,                  // Identifies the site
+                
+                site_domain : $(location).attr('host'),        // The hostname of the current page
+                site_path   : $(location).attr('pathname'),    // The page url hosting the control, 
+
+                contact_externId    : submission.customerId,   // The id of this customer at the clients site
+                contact_firstName   : submission.firstName,    // The first name of the customer
+                contact_lastName    : submission.lastName,     // The last name of the customer
+                contact_email       : submission.email,        // The email address of the customer 
+                contact_mobNumber   : submission.mobNumber,    // The mobile telephone number of the customer
+                contact_postcode    : submission.postcode,     // The mobile telephone number of the customer
+
+                answers : $.map(questionControls, function(ctrl) {
+                    return {
+                        question_id : ctrl.id, 
+                        answer      : ctrl.checked
+                    };
+                })
+            };
+
+            $.post("http://localhost:8080/api/submissions", payload, function(response) {
+
+                if (onSubmit) {
+                    onSubmit(response.submission_id);
+                }
+
+            }, "json")          
+        }
     }
 }
 
